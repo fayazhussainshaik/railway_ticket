8b53b9d037b61eb98e10ed5947379cbd85529399
fayazhussainshaik <98339019+fayazhussainshaik@users.noreply.github.com>
2022-12-07 12:06:48 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Seatbooking from "./Seatbooking";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <Seatbooking />
+  </React.StrictMode>,
+  rootElement
+);
